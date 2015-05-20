b8912315b568b725916240db683f038f174828e8
falkTX <falktx@gmail.com>
2015-05-20 16:42:18 +0200

    Add new Utils file

---
+++ twindy-1.02/src/Utils.h
@@ -0,0 +1,156 @@
+/*
+ * Utilities for Twindy
+ * Copyright (C) 2015 Filipe Coelho <falktx@falktx.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef FALKTX_TWINDY_UTILS_H_INCLUDED
+#define FALKTX_TWINDY_UTILS_H_INCLUDED
+
+#include "juce_amalgamated.h"
+#include "TwindyErrorDisplay.h"
+
+#include <sys/wait.h>
+#include <unistd.h>
+
+// -----------------------------------------------------------------------
+
+static inline
+double getSystemScaling()
+{
+    return 1.0;
+}
+
+struct Coordinates {
+    int topPadding;
+    int topTabBarHeight;
+    int topTabBarIndent;
+    int clockWidth;
+    int buttonMargin;
+    int buttonWidth;
+    int buttonHeight;
+    int errorDisplayWidth;
+    int errorDisplayHeight;
+    int leftTabsWidth;
+
+    Coordinates()
+        : topPadding(30),
+          topTabBarHeight(-1),
+          topTabBarIndent(10),
+          clockWidth(100),
+          buttonMargin(10),
+          buttonWidth(100),
+          buttonHeight(25),
+          errorDisplayWidth(200),
+          errorDisplayHeight(60),
+          leftTabsWidth(-1)
+    {
+        double scaling = getSystemScaling();
+
+        if (scaling < 0.5 || scaling > 10.0)
+        {
+            scaling = 1.0;
+
+            if (TwindyErrorDisplay* const errorDisplay = TwindyErrorDisplay::getInstance())
+                errorDisplay->addErrorMessage(TRANS("Error"), TRANS("Invalid scaling requested."));
+        }
+
+        topPadding      = double(topPadding)      * scaling;
+        topTabBarIndent = double(topTabBarIndent) * scaling;
+        clockWidth      = double(clockWidth)      * scaling;
+        buttonMargin    = double(buttonMargin)    * scaling;
+        buttonWidth     = double(buttonWidth)     * scaling;
+        buttonHeight    = double(buttonHeight)    * scaling;
+
+        topTabBarHeight = topPadding;
+        leftTabsWidth   = buttonMargin*2 + buttonWidth;
+    }
+};
+
+static inline
+const Coordinates& getCoordinates()
+{
+    static const Coordinates coord;
+    return coord;
+}
+
+// -----------------------------------------------------------------------
+
+static inline
+pid_t startProcess(const StringArray& args)
+{
+    const int    argc = args.size();
+    const char* cargs[argc+1];
+
+    for (int i=0; i<argc; ++i)
+        cargs[i] = args[i].toUTF8();
+    cargs[argc] = nullptr;
+
+    const pid_t pid = vfork();
+
+    switch (pid)
+    {
+    case 0:
+        execvp(cargs[0], const_cast<char* const*>(cargs));
+        exit(1);
+        return -1;
+
+    case -1:
+        TwindyErrorDisplay::getInstance()->addErrorMessage(TRANS("Error"),
+                                                           TRANS("Could not start external tool."));
+        return -1;
+    }
+
+    return pid;
+}
+
+static inline
+void terminateAndWaitForProcess(const pid_t pid)
+{
+    if (pid <= 0)
+        return;
+
+    bool sendTerm = true;
+
+    for (pid_t p;;)
+    {
+        p = ::waitpid(pid, nullptr, WNOHANG);
+
+        switch (p)
+        {
+        case 0:
+            if (sendTerm)
+            {
+                sendTerm = false;
+                ::kill(pid, SIGTERM);
+            }
+            break;
+
+        case -1:
+            if (errno == ECHILD)
+                return;
+            break;
+
+        default:
+            if (p == pid)
+                return;
+            break;
+        }
+
+        // 1sec
+        usleep(1*1000*1000);
+    }
+}
+
+// -----------------------------------------------------------------------
+
+#endif // FALKTX_TWINDY_UTILS_H_INCLUDED
