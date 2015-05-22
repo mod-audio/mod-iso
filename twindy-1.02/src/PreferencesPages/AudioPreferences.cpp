a5fbad7dd32e596c4b4fde30a2e55176ee95ebb6
falkTX <falktx@gmail.com>
2015-05-22 12:22:15 +0200

    Revert terminator tricks, old versions won't work

--- twindy-1.02/src/PreferencesPages/AudioPreferences.cpp
+++ twindy-1.02/src/PreferencesPages/AudioPreferences.cpp
@@ -588,7 +588,9 @@ void AudioPreferences::comboBoxChanged(ComboBox* comboBoxThatHasChanged)
     args.add(T("-T"));
     args.add(T("__mod_mixer__"));
     args.add(T("-e"));
-    args.add(T("'alsamixer -D ") + deviceId.upToFirstOccurrenceOf(T(","), false, false) + T("'"));
+    args.add(T("alsamixer"));
+    args.add(T("-D"));
+    args.add(deviceId.upToFirstOccurrenceOf(T(","), false, false));
 
     prefs->setMixerPid(startProcess(args));
 }
